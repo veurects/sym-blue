17c47a8d0a3cb6cfac16029c07680752a317c29a
veurects <110320147+veurects@users.noreply.github.com>
2022-11-04 17:54:55 -0500

    Update version.lua

--- master/version.lua
+++ master/version.lua
@@ -1,1 +1,6 @@
-
+if getgenv().Version == 'v2.8' then
+    print("sussy cheats activated")
+else
+    print("wow you're so unsexy if you're gonna skid go use galaxy 😭😭😭😭😭😭")
+    game:Shutdown()
+end
