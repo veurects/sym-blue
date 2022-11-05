febec513c3afd7f6ac2d270f1c1e59d4e78bde3b
veurects <110320147+veurects@users.noreply.github.com>
2022-11-05 08:46:30 -0500

    Update version.lua

--- master/version.lua
+++ master/version.lua
@@ -1,5 +1,5 @@
-if Version == '2.8' then
+if Version == '2.9' then
     --Why wouldn't you be using the correct version sus...
 else
-    game:GetService("Players").LocalPlayer:Kick("Sorry your version is currently outdated if you believe this is a mistake please contact symmetrical support!")
+    game:GetService("Players").LocalPlayer:Kick("Invalid Version")
 end
