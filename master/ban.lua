812c6574ae23274e36a58754c3224d26051a17a5
veurects <110320147+veurects@users.noreply.github.com>
2022-11-05 09:00:33 -0500

    Update ban.lua

--- master/ban.lua
+++ master/ban.lua
@@ -1,8 +1,3 @@
-local ban = {
+return = {
     '6817DE98-B614-4087-A6A1-0995A94D6820',
 }
-
-if table.find(ban,game:GetService("RbxAnalyticsService"):GetClientId()) then
-    blacklisted()
-    game:Shutdown()
-end
