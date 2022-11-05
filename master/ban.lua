324d1c4254525e934ca1f36febfb2db5a3761842
veurects <110320147+veurects@users.noreply.github.com>
2022-11-05 09:05:07 -0500

    Update ban.lua

--- master/ban.lua
+++ master/ban.lua
@@ -3,7 +3,9 @@ local ban = {
 }
 
 if table.find(ban,game:GetService("RbxAnalyticsService"):GetClientId()) then
-    webhook()
+    return {
+        webhook()
+    }
     wait()
     game:Shutdown()
 end
