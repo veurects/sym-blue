4377d1fef409fa11b34cae785ff25a6844a58938
veurects <110320147+veurects@users.noreply.github.com>
2022-11-08 15:18:10 -0600

    Update version.lua

--- master/version.lua
+++ master/version.lua
@@ -1,4 +1,4 @@
-if getgenv().Version == '3.3' then
+if getgenv().Version == '3.4' then
     --Why wouldn't you be using the correct version sus...
 else
     game:GetService("Players").LocalPlayer:Kick("Invalid Version")
